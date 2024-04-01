b218304e6b723a6d7d1ddd3766bd89f79f1a67a1
Chrissebah <christian.grotan@gmail.com>
2024-04-01 16:30:18 +0200

    finishing up the content page

--- javascriptframeworks/src/App.js
+++ javascriptframeworks/src/App.js
@@ -28,25 +28,36 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
+            <header className="header">
+        <div className="header-buttons">
+          <button>Button 1</button>
+          <button>Button 2</button>
+          <button>Button 3</button>
+        </div>
+      </header>
+      <main className="main-content">
+        <h1>My Online Shop</h1>
         {error && <p>{error}</p>}
         
         {loading ? (
           <p>Loading...</p>
         ) : (
-          <ul>
-      {items.map((item) => (
-        <li key={item.id}>
-          <div className="Title">{item.title}</div>
-          <img src={item.image.url} alt={item.title} /> 
-          <div className='Price'>Full Price: {item.price}</div>
-          <div className='discountedPrice'>Discounted Price: {item.discountedPrice}</div>
-          <div className="ID">ID: {item.id}</div>
-        </li>
-      ))}
-    </ul>
+          <ul className="item-list">
+            {items.map((item) => (
+              <li key={item.id} className="item">
+                <div className="Title">{item.title}</div>
+                <img src={item.image.url} alt={item.title} /> 
+                <div className='Price'>Full Price: {item.price}</div>
+                <div className='discountedPrice'>Discounted Price: {item.discountedPrice}</div>
+                <div className="ID">ID: {item.id}</div>
+              </li>
+            ))}
+          </ul>
         )}
-      </header>
+      </main>
+      <footer className="footer">
+        <p>&copy; 2024 My Online Shop</p>
+      </footer>
     </div>
   );
 }
